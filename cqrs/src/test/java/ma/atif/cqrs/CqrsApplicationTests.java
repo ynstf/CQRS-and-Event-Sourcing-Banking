030e4187105c0c6b70d94d60f74ef40056bf82ac
YONES <107154559+ynstf@users.noreply.github.com>
2025-11-27 16:34:51 +0100

    init project

---
+++ cqrs/src/test/java/ma/atif/cqrs/CqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.atif.cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
